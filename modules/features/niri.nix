0108ac49830f54fbba16c06ffd00e062bb5a20fb
jongleur <arthurdraye@gmail.com>
2026-03-26 22:11:27 +0100

    new niri

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -199,4 +199,11 @@
       imports = [self.wrapperModules.niri];
     };
   };
+
+  flake.nixosModules.niri = { pkgs, lib, ... }: {
+    programs.niri = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+    };
+  };
 }
