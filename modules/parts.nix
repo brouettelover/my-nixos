eaa585d85bcec536fff36b713080ef735825de55
jongleur <arthurdraye@gmail.com>
2026-03-26 22:37:42 +0100

    niri new config

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,4 +1,12 @@
+{ inputs, ... }:
 {
+  options =  {
+    flake = inputs.flake-parts.lib.mkSubmoduleOptions {
+      wrapperModules = inputs.nixpkgs.lib.mkOption {
+        default = {};  
+      };
+    };
+  };
   config = {
     systems = [
       "x86_64-linux"
