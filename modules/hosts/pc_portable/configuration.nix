ed92d0e2936152b461d89e0ddcb87db6b9b3836a
jongleur <arthurdraye@gmail.com>
2026-03-26 13:51:29 +0100

    new nixos config

--- modules/hosts/pc_portable/configuration.nix
+++ modules/hosts/pc_portable/configuration.nix
@@ -12,9 +12,13 @@
     environment.systemPackages = with pkgs; [
       firefox
       vim
+      wget
+      btop
+      bat
+      git
 
     ];
-    
+     
     boot = {
       loader.grub.enable = true;
       loader.grub.efiSupport = true;
@@ -24,10 +28,81 @@
 
       kernelParams = ["quiet"];
       kernelModules = [ "kvm_amd" "coretemp" "cpuid" "v4l2loopback" ];
-      
+      #cotemp -> temperature
+      #kvm_amd -> pour qemu
+      #v4l2 -> option dans teams,zoom,...
 
-
+      plymouth.enable = true; # animation au boot
     };
+    
+    security.polkit.enable = true;
+    
+    users.users.jongleur = {
+      isNormalUser = true;
+      description = "jongleur";
+      extraGroups = [ "networkmanager" "wheel" ];
+    };
+
+    networking = {
+      hostname = "pc-portable";
+      networkmanager.enable = true;
+    };
+
+
+    # DON T KNOW IF IT USEFULL
+    # Select internationalisation properties.
+    i18n.defaultLocale = "fr_BE.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "fr_BE.UTF-8";
+      LC_IDENTIFICATION = "fr_BE.UTF-8";
+      LC_MEASUREMENT = "fr_BE.UTF-8";
+      LC_MONETARY = "fr_BE.UTF-8";
+      LC_NAME = "fr_BE.UTF-8";
+      LC_NUMERIC = "fr_BE.UTF-8";
+      LC_PAPER = "fr_BE.UTF-8";
+      LC_TELEPHONE = "fr_BE.UTF-8";
+      LC_TIME = "fr_BE.UTF-8";
+    };
+
+    # Configure console keymap
+    console.keyMap = "be-latin1";
+    #######
+    hardware.bluetooth.enable = true;
+    hardware.cpu.amd.updateMicrocode = true;
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    boot.initrd.kernelModules = [ "amdgpu" ];
+    services = {
+      #blueman.enable = true; #Bluetooth_GUI
+      printing.enable = true; #Imprimante
+      xserver.videoDrivers = [ "amdgpu" ];
+    };
+    
+    xdg.portal.extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+    ];
+    xdg.portal.enable = true;
+    xdg.portal.config.common.default = [ "gnome" "gtk" ]; 
+
+    nix = {
+      settings.auto-optimise-store = true; # Saves space by hardlinking files
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
+    };
+    
+
+    programs.niri.enable = true;
+
+    time.timeZone = "Europe/Brussels";
+ 
+    system.stateVersion = "25.11";
   };
 
 }
