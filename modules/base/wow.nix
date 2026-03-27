539da58e28ad571f5481d676b9c12dd36f1a4734
jongleur <arthurdraye@gmail.com>
2026-03-27 09:16:31 +0100

    adding wow

--- modules/base/wow.nix
+++ modules/base/wow.nix
@@ -2,12 +2,13 @@
 
 {
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    GAMEMODE_PATH = "$HOME/.local/share/gamemode";
-  };
-  
+ 
   flake.nixosModules.wow = { pkgs, ... }: {
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      GAMEMODE_PATH = "$HOME/.local/share/gamemode";
+    };
+ 
     environment.systemPackages = [
         pkgs.lutris
         pkgs.protonup-qt
