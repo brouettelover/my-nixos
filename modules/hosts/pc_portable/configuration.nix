2a2f619b42046bce75e24e8a3a45816e45d973df
jongleur <arthurdraye@gmail.com>
2026-03-30 19:01:33 +0200

    adding wow

--- modules/hosts/pc_portable/configuration.nix
+++ modules/hosts/pc_portable/configuration.nix
@@ -6,7 +6,7 @@
       self.nixosModules.pc_portable_hardware_configuration
       self.nixosModules.niri
       self.nixosModules.fish
-      #self.nixosModules.wow
+      self.nixosModules.wow
       self.nixosModules.neovim
       self.nixosModules.kitty
       self.nixosModules.pwned
