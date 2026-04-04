a12488b060a1f830e871ad9a2c7499d1f3370676
jongleur <arthurdraye@gmail.com>
2026-04-04 11:27:21 +0200

    adding battery

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -2,6 +2,7 @@
   perSystem = { pkgs, ... }: {
     packages.noctalia-shell = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
+      extraPackages = [ pkgs.acpi ];
       settings =
         (builtins.fromJSON
           (builtins.readFile ./noctalia.json)).settings;
